2abec7add3cf85e707080aeef251b2df56ef54d6
Agata Kowalska <agatakowalska72@wp.pl>
2021-05-28 18:34:16 +0200

    Drugi commit - koniec zadania

--- src/PointAplication.java
+++ src/PointAplication.java
@@ -3,12 +3,12 @@ public class PointAplication {
         Point point1 =new Point(5,6);
         PointController operation = new PointController();
 
-        System.out.println("Punkt x: "+ point1.x + " Point y: " + point1.y);
-
+        point1.print();
         operation.addX(point1);
 
         point1.print();
         operation.minusY(point1);
+
         point1.print();
 
 
